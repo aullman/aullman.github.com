2dfa2adc458ee186947da2ebf38d0e010ee5b64f
Adam Ullman <adam@tokbox.com>
2013-05-19 16:22:19 -0700

    First version of the slides for WebRTC Developer Meetup

---
+++ webrtc-developer-meetup/js/app.js
@@ -0,0 +1,10 @@
+angular.module('slideviewer', []).
+    config(['$routeProvider', function($routeProvider) {
+      $routeProvider.
+            when('/slide/14', {redirectTo: '/slide/1'}).
+            when('/slide/:slideNum', {templateUrl:'slides/slide.html', controller: SlideCtrl}).
+            otherwise({redirectTo: '/slide/1'});
+    }]).
+    directive('publisher', TB.angular.PublisherDirective).
+    directive('subscriber', TB.angular.SubscriberDirective);
+    
---
+++ webrtc-developer-meetup/js/controllers.js
@@ -0,0 +1,52 @@
+function SlideCtrl($scope, $routeParams) {
+    $scope.slideNum = parseInt($routeParams.slideNum, 10);
+}
+
+var OpenTokCtrl = TB.angular.createOpentokCtrl(1127, 
+            "1_MX4xMTI3fn5UdWUgTWF5IDE0IDE3OjM0OjMyIFBEVCAyMDEzfjAuNTI4ODU1NH4", 
+            "T1==cGFydG5lcl9pZD0xMTI3JnNka192ZXJzaW9uPXRicHktdjAuOTEuMjAxMS0wNy0wNSZzaWc9OWZlYmIxMGQ2ZTJmODkzNzE4YTIxZDk5ZWYzZGFiNWYzZWVjZjdjNTpleHBpcmVfdGltZT0xMzcxMjA2MDczJm5vbmNlPTc0MTA5MyZjcmVhdGVfdGltZT0xMzY4NTc4MDczJnJvbGU9bW9kZXJhdG9yJnNlc3Npb25faWQ9MV9NWDR4TVRJM2ZuNVVkV1VnVFdGNUlERTBJREUzT2pNME9qTXlJRkJFVkNBeU1ERXpmakF1TlRJNE9EVTFOSDQ=");
+
+function SlidesCtrl($scope, $routeParams, $location) {
+    var slidesRef = new Firebase('https://webrtc-meetup.firebaseIO.com/slideNum');
+    
+    var getSlideNum = function() {
+        return parseInt($location.$$path.match("\/slide\/(.+)")[1], 10);
+    };
+    
+    var setSlideNum = function(slideNum, push) {
+        $scope.$apply(function() {
+            $scope.slideNum = slideNum;
+            $location.path("/slide/" + $scope.slideNum);
+            
+            if (push) slidesRef.set(slideNum);
+        });
+    };
+    
+    var slideNumUpdated = function(snapshot) {
+        if (snapshot.val()) setSlideNum(parseInt(snapshot.val(), 10), false);
+    };
+    
+    slidesRef.on("value", slideNumUpdated);
+    
+    $scope.slideNum = getSlideNum();
+    
+    var arrowControls = function(event) {
+        var slideNum;
+        if (event.keyCode === 37) {
+            slideNum = getSlideNum() - 1;
+        } else if (event.keyCode === 39) {
+            slideNum = getSlideNum() + 1;
+        }
+        
+        if (slideNum) {
+            setSlideNum(slideNum, true);
+        }
+    };
+    
+    angular.element(document.body).bind("keydown", arrowControls);
+    
+    $scope.$on("$destroy", function() {
+        angular.element(document.body).unbind("keydown", arrowControls);
+        slidesRef.off("value", slideNumUpdated);
+    });
+}
