d874d9da3611f939b041460564ca8c8b24caabf6
Adam Ullman <adam@tokbox.com>
2013-05-19 17:03:36 -0700

    Added transitions to the slides.

--- webrtc-developer-meetup/js/controllers.js
+++ webrtc-developer-meetup/js/controllers.js
@@ -1,12 +1,21 @@
 function SlideCtrl($scope, $routeParams) {
-    $scope.slideNum = parseInt($routeParams.slideNum, 10);
+    //$scope.slideNum = parseInt($routeParams.slideNum, 10);
 }
 
 var OpenTokCtrl = TB.angular.createOpentokCtrl(1127, 
             "1_MX4xMTI3fn5UdWUgTWF5IDE0IDE3OjM0OjMyIFBEVCAyMDEzfjAuNTI4ODU1NH4", 
             "T1==cGFydG5lcl9pZD0xMTI3JnNka192ZXJzaW9uPXRicHktdjAuOTEuMjAxMS0wNy0wNSZzaWc9OWZlYmIxMGQ2ZTJmODkzNzE4YTIxZDk5ZWYzZGFiNWYzZWVjZjdjNTpleHBpcmVfdGltZT0xMzcxMjA2MDczJm5vbmNlPTc0MTA5MyZjcmVhdGVfdGltZT0xMzY4NTc4MDczJnJvbGU9bW9kZXJhdG9yJnNlc3Npb25faWQ9MV9NWDR4TVRJM2ZuNVVkV1VnVFdGNUlERTBJREUzT2pNME9qTXlJRkJFVkNBeU1ERXpmakF1TlRJNE9EVTFOSDQ=");
 
-function SlidesCtrl($scope, $routeParams, $location) {
+function SlidesCtrl($scope, $routeParams, $location, $window) {
+    $scope.slides = OT._.map([1,2,3,4,5,6,7,8,9,10,11,12,13], function(num) {
+        return "slides/" + num + ".html";
+    });
+    
+    $($window).on("resize", function() {
+        $scope.$apply(function() {
+            $scope.slideWidth = $window.innerWidth;
+        });
+    });
     var slidesRef = new Firebase('https://webrtc-meetup.firebaseIO.com/slideNum');
     
     var getSlideNum = function() {
